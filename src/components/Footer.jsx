02a0ffe37202e4112f05c217ee0bcf1fae9e298c
Anastasia <99129924+AnastasiyaZvorskaya@users.noreply.github.com>
2022-09-11 09:34:15 +0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,10 +16,10 @@ const Footer = () => {
             You are welcome!</p>
 <Button/>
 <div className='network_img'>
-    <NavLink to='https://www.linkedin.com/in/anastasiya-z-825230230/'><Image src={require('../Image/In.png')} /></NavLink>
-    <NavLink to='https://www.instagram.com/anastasiiazvorskaya/'><Image src={require('../Image/insta.png')} /></NavLink>
-    <NavLink to='https://github.com/AnastasiyaZvorskaya'><Image src={require('../Image/git.PNG')} /></NavLink>
-    <NavLink to='https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox'><Image src={require('../Image/mail.png')} /></NavLink>
+    <a href='https://www.linkedin.com/in/anastasiya-z-825230230/'><Image src={require('../Image/In.png')} /></a>
+    <a href='https://www.instagram.com/anastasiiazvorskaya/'><Image src={require('../Image/insta.png')} /></a>
+    <a href='https://github.com/AnastasiyaZvorskaya'><Image src={require('../Image/git.PNG')} /></a>
+    <a href='https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox'><Image src={require('../Image/mail.png')} /></a>
 </div>
 <p className='footer_network'>Like me on <br></br>
 LinkedIn, Instagram, GitHub, Gmail</p>
